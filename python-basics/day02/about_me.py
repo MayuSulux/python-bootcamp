330131f221a9b459b32f8a92002e8dafcf5dee45
Mayuri Suluxshan <mayurisuluxshan@Mayuris-MBP.lobstershack>
2026-07-31 12:07:41 +1000

    Day 2: Variables, data types and f-strings

---
+++ python-basics/day02/about_me.py
@@ -0,0 +1,12 @@
+name = "Mayuri"
+age = 38
+occupation = "Assistant cafe Manager"
+country ="Australia"
+dream_job ="AI Engineer"
+
+print(f"Hello! My name is {name}.")
+print(f"I am {age} years old.")
+print(f"Currently, I am living in {country}.")
+print(f"I work as an {occupation}.")
+print(f"But my dream job is {dream_job}")
+print("I hope that i will get my dream job soon")
---
+++ python-basics/day02/variables.py
@@ -0,0 +1,19 @@
+# Day 2 variables
+
+name = "Mayuri"
+age = 38
+country = "Australia"
+occupation = "Assistant cafe Manager"
+
+print("Name:", name )
+print("Age:", age)
+print("Country:", country)
+print("Occupation:", occupation)
+
+height = 158.5
+is_learning = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_learning))
