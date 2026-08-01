78fc6ea1b9a3da3c861e43a0d9170d8c8e3cc400
Mayuri Suluxshan <mayurisuluxshan@Mayuris-MBP.lobstershack>
2026-08-01 19:06:23 +1000

    Day 3 - User input program

---
+++ python-basics/day03/about_you.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+age = input("How aold are you?")
+Country = input("what is ypur country?")
+Dreamjob = input("what is your dream job?")
+
+print(f"Helo {name}")
+print(f"{age} years old")
+print(f"I'm from {Country}")
+print(f"My dream job is {Dreamjob}")
