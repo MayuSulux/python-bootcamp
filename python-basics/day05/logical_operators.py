dba362454a1e07ca1a8071c0075581ef442dbc8a
Mayuri Suluxshan <mayurisuluxshan@Mayuris-MBP.lobstershack>
2026-08-07 15:16:42 +1000

    Day05: Logical Operators

---
+++ python-basics/day05/logical_operators.py
@@ -0,0 +1,8 @@
+mark = int(input("What is your mark? "))
+attendance = int(input("What is your attendance? "))
+
+
+if mark >=75 and attendance >=90:
+ print("Eligible for a Scholarship")
+else:
+ print("Not Eligible")
